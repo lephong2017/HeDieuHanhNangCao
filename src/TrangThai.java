4fe0ada3091e13ebccea9b36f3ebd8dcef3f24a6
AnhMap <nguyenlephong1997@gmail.com>
2019-01-11 20:52:30 +0700

    upload code project HDH advanced 5 nha triet gia

---
+++ src/TrangThai.java
@@ -0,0 +1,16 @@
+
+
+public enum TrangThai {
+	DOIBUNG("doi bung"), DANGAN("dang an"), DANGSUYNGHI("dang suy nghi");
+	
+	private final String str;
+	
+	TrangThai(String str) {
+		this.str = str;
+	}
+	
+	@Override
+	public String toString() {
+		return str;
+	}
+}
